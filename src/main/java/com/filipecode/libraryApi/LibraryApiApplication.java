bb31275c593c691011343f928b701f01b8ada080
Filipe Mota <filipera3@gmail.com>
2025-04-02 10:07:16 -0300

    feat: add JpaAuditing in Author
    timestamp da data de criação e atualização do autor automaticamente

--- src/main/java/com/filipecode/libraryApi/LibraryApiApplication.java
+++ src/main/java/com/filipecode/libraryApi/LibraryApiApplication.java
@@ -2,8 +2,10 @@ package com.filipecode.libraryApi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing // Para o EntityListeners funcionar
 public class LibraryApiApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/filipecode/libraryApi/model/entities/Author.java
+++ src/main/java/com/filipecode/libraryApi/model/entities/Author.java
@@ -4,8 +4,12 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.UUID;
 
@@ -14,6 +18,7 @@ import java.util.UUID;
 @Getter
 @Setter
 @ToString(exclude = "books")
+@EntityListeners(AuditingEntityListener.class) // Classe fica escutando sempre que eu faço uma alteração nessa entidade
 public class Author {
 
     @Id
@@ -32,4 +37,15 @@ public class Author {
 
     @OneToMany(mappedBy = "author", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
     private List<Book> books;
+
+    @CreatedDate
+    @Column(name = "data_cadastro")
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(name = "data_atualizacao")
+    private LocalDateTime updatedAt;
+
+    @Column(name = "id_usuario")
+    private UUID idUsuer;
 }
