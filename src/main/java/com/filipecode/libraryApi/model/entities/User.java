9c4e333acdbd1c01281e6e2730f7e2474cf1bf1b
filipedev <filipeddev@gmail.com>
2025-06-16 14:52:03 -0300

    feat: add user entity

---
+++ src/main/java/com/filipecode/libraryApi/model/entities/User.java
@@ -0,0 +1,31 @@
+package com.filipecode.libraryApi.model.entities;
+
+import io.hypersistence.utils.hibernate.type.array.ListArrayType;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.Type;
+
+import java.util.List;
+import java.util.UUID;
+
+@Entity
+@Table(name = "usuario")
+@Getter
+@Setter
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column
+    private String login;
+
+    @Column
+    private String password;
+
+    @Type(ListArrayType.class) // Fazer a tradução de List para Array
+    @Column(name = "roles", columnDefinition = "varchar[]")
+    private List<String> roles;
+}
