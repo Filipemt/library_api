f86cbcce3c9e5e96ffc246a7cd564756e93627ae
filipedev <filipeddev@gmail.com>
2025-06-16 11:10:31 -0300

    feat: add user roles
    - Aprendendo sobre o uso das roles dentro de um sistema; Um grupo de usuários
    - Aprendendo sobre Authority; Uma permissão para executar uma tarefa

--- src/main/java/com/filipecode/libraryApi/config/SecurityConfiguration.java
+++ src/main/java/com/filipecode/libraryApi/config/SecurityConfiguration.java
@@ -2,6 +2,7 @@ package com.filipecode.libraryApi.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -28,6 +29,15 @@ public class SecurityConfiguration {
                 })
                 .httpBasic(Customizer.withDefaults())
                 .authorizeHttpRequests(authorize -> {
+
+//                    authorize.requestMatchers(HttpMethod.POST, "/autores/**").hasRole("ADMIN");
+//                    authorize.requestMatchers(HttpMethod.DELETE, "/autores/**").hasRole("ADMIN");
+//                    authorize.requestMatchers(HttpMethod.PUT, "/autores/**").hasRole("ADMIN");
+//                    authorize.requestMatchers(HttpMethod.GET, "/autores/**").hasAnyRole("USER" ,"ADMIN");
+
+                    authorize.requestMatchers("/login").permitAll();
+                    authorize.requestMatchers("/autores/**").hasRole("ADMIN");
+                    authorize.requestMatchers("/livros/**").hasAnyRole("USER", "ADMIN");
 
                     authorize.anyRequest().authenticated();
                 })
