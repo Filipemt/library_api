fcc9c1888d45f470c5a3004a9b7117ab8963bd7e
Filipe Mota <filipera3@gmail.com>
2025-06-23 11:18:00 -0300

    feat: add CustomAuthentication

---
+++ src/main/java/com/filipecode/libraryApi/security/CustomAuthentication.java
@@ -0,0 +1,57 @@
+package com.filipecode.libraryApi.security;
+
+
+import com.filipecode.libraryApi.model.entities.UserLogin;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+
+import java.util.Collection;
+import java.util.stream.Collectors;
+
+@RequiredArgsConstructor
+@Getter
+public class CustomAuthentication implements Authentication {
+
+    private final UserLogin user;
+
+    @Override
+    public Collection<GrantedAuthority> getAuthorities() {
+        return this.user.getRoles()
+                .stream()
+                .map(role -> new SimpleGrantedAuthority(role))
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public Object getCredentials() {
+        return null;
+    }
+
+    @Override
+    public Object getDetails() {
+        return user;
+    }
+
+    @Override
+    public Object getPrincipal() {
+        return user;
+    }
+
+    @Override
+    public boolean isAuthenticated() {
+        return false;
+    }
+
+    @Override
+    public void setAuthenticated(boolean isAuthenticated) throws IllegalArgumentException {
+
+    }
+
+    @Override
+    public String getName() {
+        return user.getLogin();
+    }
+}
