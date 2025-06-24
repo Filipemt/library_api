ad2ac823c3a02662a0dc78981f9ef72477e29243
Filipe Mota <filipera3@gmail.com>
2025-06-24 07:58:52 -0300

    refactor: remove useless imports

--- src/main/java/com/filipecode/libraryApi/security/SecurityService.java
+++ src/main/java/com/filipecode/libraryApi/security/SecurityService.java
@@ -1,18 +1,14 @@
 package com.filipecode.libraryApi.security;
 
 import com.filipecode.libraryApi.model.entities.UserLogin;
-import com.filipecode.libraryApi.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
 public class SecurityService {
-
-    private final UserService userService;
 
     public UserLogin getByLoggedUser() {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
